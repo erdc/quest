286a7bdec969a4b2b81a31202a3929b22f9d1b1a
Dharhas Pothina <dharhas@gmail.com>
2014-11-10 12:15:43 -0600

    add chesapeake bay demo service. note need to place test_chesapeake.vtk in dsl demo dir under a subfolder called chesapeake

---
+++ data_services_library/services/chesapeake_demo.py
@@ -0,0 +1,47 @@
+"""
+Example Services
+"""
+
+from data_services_library.services import base
+import fiona
+from geojson import Feature, FeatureCollection, Point, Polygon
+from random import random
+import os
+
+
+class ChesapeakeVTK(base.DataServiceBase):
+    def register(self):
+        """Register Chesapeake Demo VTK service 
+        """
+        self.metadata = {
+                    'provider': {'name': 'Chesapeake Elevation', 'id': 'chesapeake-vtk'},
+                    'dataset_name': 'Chesapeake Elevation',
+                    'description': 'Chesapeake Elevation',
+                    'geographical area': 'Chesapeake Bay',
+                    'bbox': [-78., 36., -74., 41.],
+                    'geotype': 'polygons',
+                    'type': 'raster'
+                }
+
+
+    def get_locations(self, bbox=None):
+        if not bbox:
+            bbox = self.metadata['bbox']
+
+        demo_dir = os.getenv('DSL_DEMO_DIR')
+        path = os.path.join(demo_dir, 'chesapeake', 'test_chesapeake.vtk')
+        x1, y1, x2, y2 = self.metadata['bbox']
+        polys = []
+        properties = {'uri': 'file://' + path}
+        polys.append(Feature(geometry=Polygon([_bbox2poly(x1, y1, x2, y2)]), properties=properties))
+
+        return FeatureCollection(polys)
+
+
+def _bbox2poly(x1, y1, x2, y2):
+    xmin, xmax = sorted([float(x1), float(x2)])
+    ymin, ymax = sorted([float(y1), float(y2)])
+    poly = [(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax, ymin)]
+    poly.append(poly[0])
+
+    return poly
