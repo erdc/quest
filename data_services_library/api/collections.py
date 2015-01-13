f871593d7fade638dac8db906479fe77119d6359
Dharhas Pothina <dharhas@gmail.com>
2015-01-13 13:15:10 -0600

    add functionality to collections api

--- data_services_library/api/collections.py
+++ data_services_library/api/collections.py
@@ -2,15 +2,30 @@
 api functions related to handling collections
 """
 from .. import util
+import datetime
 import json
 import os
 
 COLLECTIONS_FILE = 'collections.json'
+COLLECTION_METADATA_FILE = 'collection.json'
 METADATA_FILE = 'dsl.json'
 
 
-def add_to_collection(name, data):
-	pass
+def add_to_collection(name, service, features):
+	collection = retrieve_collection(name)
+	dataset = collection['datasets'].get(service)
+	if not dataset:
+		dataset = { 
+			'features': features,
+			'downloaded': False
+		}
+		collection['datasets'][service] = dataset
+	else:
+		dataset['features'] = _append_features(dataset['features'], features)
+		dataset['downloaded'] = False
+	
+	_write_collection(collection)
+	return collection
 
 
 def create_collection(name, path=None, tags=None):
@@ -27,6 +42,11 @@ def create_collection(name, path=None, tags=None):
 	util.mkdir_if_doesnt_exist(collection_path)
 
 	collection = {'path': collection_path}
+	metadata = {'created_on' : datetime.datetime.now().isoformat(), 'datasets': {}}
+
+	with open(os.path.join(collection['path'], COLLECTION_METADATA_FILE), 'w') as f:
+		json.dump(metadata, f)
+
 	collections[name] = collection
 	_write_collections(collections)
 
@@ -50,15 +70,42 @@ def list_collections():
 
 
 def retrieve_collection(name):
-	pass
+	collection = _load_collections()[name]
+	with open(os.path.join(collection['path'], COLLECTION_METADATA_FILE)) as f:
+		collection.update(json.load(f))
+
+	collection['name'] = name
+	return collection
 
 
 def update_collection(name):
     pass
 
 
-def delete_from_collection(name, feature_id):
-    pass
+def delete_from_collection(name, service, feature_ids):
+	if not isinstance(feature_ids, list):
+		feature_ids = [feature_ids]
+
+	collection = retrieve_collection(name)
+	dataset = collection['datasets'].get(service)
+	if dataset:
+		dataset['features']['features'] = [feature for feature in dataset['features']['features'] if feature['id'] not in feature_ids]
+    
+    	if not dataset['features']['features']:
+    		del collection['datasets'][service]
+
+		_write_collection(collection)
+
+	return collection
+
+
+def _append_features(old, new):
+	existing_features = [feature['id'] for feature in old['features']]
+	for feature in new['features']:
+		if feature['id'] not in existing_features:
+			old['features'].append(feature)
+
+	return old
 
 
 def _load_collections():
@@ -69,6 +116,11 @@ def _load_collections():
 
 	with open(path) as f:
 		return json.load(f)
+
+
+def _write_collection(collection):
+	with open(os.path.join(collection['path'], COLLECTION_METADATA_FILE), 'w') as f:
+		json.dump(collection, f)
 
 
 def _write_collections(collections):
