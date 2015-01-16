918621e70d75568c62eaad70e047606deb41acee
Dharhas Pothina <dharhas@gmail.com>
2015-01-16 11:55:30 -0600

    move driver loading into a util fn. ad a jsonify decorator that allows use of json and as_json to supply args/get return as json string

--- data_services_library/util.py
+++ data_services_library/util.py
@@ -1,5 +1,10 @@
 import appdirs
 import os
+from stevedore import extension, driver
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
 
 def get_dsl_dir(sub_dir=None):
@@ -24,3 +29,27 @@ def mkdir_if_doesnt_exist(dir_path):
     """makes a directory if it doesn't exist"""
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
+
+
+def load_drivers(namespace, names):
+    if isinstance(names, list):
+        drivers = extension.ExtensionManager(
+            namespace=FILTERS_NAMESPACE,
+            invoke_on_load=True,
+        )
+    else:
+        drivers = driver.DriverManager(namespace, name, invoke_on_load='True')
+
+    return drivers
+
+
+def jsonify(f):
+    def wrapped_f(*args, **kw):
+        if 'json' in kw.keys():
+            kw = json.loads(kw)
+
+        if 'as_json' in kw.keys():
+            return json.dumps(f(*args, **kw), sort_keys=True, indent=4, separators=(',', ': '))
+        else:
+            return f(*args, **kw)
+    return wrapped_f  # the wrapped function gets returned.
