af949d19fe2b454002e1c59c6cca9abe5523f294
Dharhas Pothina <dharhas@gmail.com>
2015-01-30 14:17:10 -0600

    convert yaml to python dict

--- config.py
+++ config.py
@@ -1,25 +1,23 @@
-services:
-    usgs-nwis-iv: dsl.services.usgs_nwis:UsgsNwisIV
-    usgs-nwis-dv: dsl.services.usgs_nwis:UsgsNwisDV
-    usgs-ned-1: dsl.services.usgs_ned:UsgsNed1ArcSecond
-    usgs-ned-13: dsl.services.usgs_ned:UsgsNed13ArcSecond
-    usgs-ned-19: dsl.services.usgs_ned:UsgsNed19ArcSecond
-    usgs-ned-2: dsl.services.usgs_ned:UsgsNed2ArcSecond
-    usgs-eros-nlcd2001: dsl.services.usgs_eros:UsgsErosNlcd2001
-    usgs-eros-nlcd2006: dsl.services.usgs_eros:UsgsErosNlcd2006
-    ncdc-ghcn: dsl.services.ncdc_ghcn:NcdcGhcn
-    ncdc-gsod: dsl.services.ncdc_gsod:NcdcGsod
-    example-points: dsl.services.example:ExamplePoints
-    example-polys: dsl.services.example:ExamplePolys
-    example-vitd: dsl.services.example:ExampleVITD
-    iraq-vitd: dsl.services.iraq_demo:IraqVITD
-    iraq-agc-tlidar: dsl.services.iraq_demo:IraqAGCTLidar
-    iraq-agc-alidar: dsl.services.iraq_demo:IraqAGCALidar
-    chesapeake-elevation: dsl.services.chesapeake_demo:ChesapeakeVTK
+services = {
+        'usgs-nwis-iv': 'dsl.services.usgs_nwis:UsgsNwisIv',
+        'usgs-nwis-dv': 'dsl.services.usgs_nwis:UsgsNwisDv',
+        #usgs-ned-1: dsl.services.usgs_ned:UsgsNed1ArcSecond
+        #usgs-ned-13: dsl.services.usgs_ned:UsgsNed13ArcSecond
+        #usgs-ned-19: dsl.services.usgs_ned:UsgsNed19ArcSecond
+        #usgs-ned-2: dsl.services.usgs_ned:UsgsNed2ArcSecond
+        #usgs-eros-nlcd2001: dsl.services.usgs_eros:UsgsErosNlcd2001
+        #usgs-eros-nlcd2006: dsl.services.usgs_eros:UsgsErosNlcd2006
+        #ncdc-ghcn: dsl.services.ncdc_ghcn:NcdcGhcn
+        #ncdc-gsod: dsl.services.ncdc_gsod:NcdcGsod
+        #example-points: dsl.services.example:ExamplePoints
+        #example-polys: dsl.services.example:ExamplePolys
+        #example-vitd: dsl.services.example:ExampleVITD
+        #iraq-vitd: dsl.services.iraq_demo:IraqVITD
+        #iraq-agc-tlidar: dsl.services.iraq_demo:IraqAGCTLidar
+        #iraq-agc-alidar: dsl.services.iraq_demo:IraqAGCALidar
+        #chesapeake-elevation: dsl.services.chesapeake_demo:ChesapeakeVTK
+    }
 
-
-collections:
-    usgs: []
-
-filters:
-    vitd-basic: dsl.filters.vitd:Basic
+filters = {
+        'vitd-basic': 'dsl.filters.vitd:Basic',
+    }
