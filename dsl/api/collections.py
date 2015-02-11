105df9773f3478ec0354aab1b89864ee01f2e587
Dharhas Pothina <dharhas@gmail.com>
2015-02-11 08:38:37 -0600

    add docstrings for collections

--- dsl/api/collections.py
+++ dsl/api/collections.py
@@ -13,10 +13,31 @@ METADATA_FILE = 'dsl.json'
 
 
 def add_to_collection(name, service, locations, parameters=None):
-	"""
+	"""adds locations from a service to a collection
 
-		locations are location ids
-		parameters None or comma separated
+	Note: Data is not downloaded in this function call. Placeholders
+	are placed in the collection. To download data call function
+	download_in_collection
+
+	Parameters
+	----------
+	name : str,
+		The name of the collection 
+	service : str,
+		The name of the service
+	locations : str,
+		comma separated list of locations. These are location codes or ids 
+		of the locations to be added from a service
+	parameters : ``None`` or str,
+		comma separated list of parameters to download for each location. If 
+		``None`` (default) is passed then all available parameters for that 
+		service will be used.
+
+	Returns
+    -------
+    collection : dict, 
+    	A python dict representation of the collection, the collection is also 
+    	written to a json file.
 	"""
 	collection = get_collection(name)
 	
@@ -48,7 +69,23 @@ def add_to_collection(name, service, locations, parameters=None):
 
 
 def download_in_collection(name):
-	"""For now just downloaded all in collection
+	"""download data for services/locations/parameters present in collection
+
+	Currently *all* datasets present in a collection will be downloaded with 
+	default download options. The data is downloaded to subfolders in the 
+	collection folder and a relative path reference is placed in the collection
+	metadata. A separate file is saved for each service/location/parameter tuple.
+
+	Parameters
+	----------
+	name : str,
+		The name of the collection
+
+	Returns
+	-------
+    collection : dict, 
+    	A python dict representation of the collection, the collection is also 
+    	written to a json file.
 
 	"""
 	collection = get_collection(name)
@@ -63,6 +100,26 @@ def download_in_collection(name):
 
 
 def new_collection(name, path=None, tags=None):
+	"""create a new collection
+
+	Create a new collection by creating a new folder and placing a json
+	file in the folder for dsl metadata and adding a reference to the 
+	master collections metadata folder.
+
+	Parameters
+	----------
+	name : str,
+		The name of the collection
+	path: ``None`` or str,
+        If ``None`` use default dsl location for collections otherwise use specified path. 
+
+	Returns
+	-------
+    collection : dict,
+    	A python dict representation of the collection, the collection is also 
+    	written to a json file.
+	"""
+
 	collections = _load_collections()
 
 	if name in collections.keys():
@@ -88,6 +145,25 @@ def new_collection(name, path=None, tags=None):
 
 
 def delete_collection(name):
+	"""delete a collection
+
+	Deletes a collection from the collections metadata file.
+
+	Note: This currently does not delete folders and/or data present 
+	in the collection folder, it just removes the reference from the
+	master metadata file.
+
+	Parameters
+	----------
+	name : str,
+		The name of the collection
+
+	Returns
+	-------
+    collections : dict,
+    	A python dict representation of the list of available collections, 
+    	the updated collections list is also written to a json file.
+	"""
 	collections = _load_collections()
 
 	if not name in collections.keys():
@@ -100,10 +176,33 @@ def delete_collection(name):
 
 
 def list_collections():
+	"""list all available collections
+
+	Returns
+	-------
+    collections : dict,
+    	A python dict representation of the list of available collections, 
+    	the updated collections list is also written to a json file.
+	"""
 	return _load_collections()
 
 
 def get_collection(name):
+	"""get a collection
+
+	Retreives collection metadata as a python dict
+
+	Parameters
+	----------
+	name : str,
+		The name of the collection
+
+	Returns
+	-------
+	collection : dict,
+    	A python dict representation of the collection, the collection is also 
+    	written to a json file.
+	"""
 	collection = _load_collections()[name]
 	with open(os.path.join(collection['path'], COLLECTION_METADATA_FILE)) as f:
 		collection.update(json.load(f))
@@ -113,10 +212,18 @@ def get_collection(name):
 
 
 def update_collection(name):
+	"""Not sure of function, maybe allow moving to different path etc
+
+	NOT IMPLEMENTED
+	"""
     raise NotImplementedError('Updating Collections has not been implemented')
 
 
 def delete_from_collection(name, service, feature_ids):
+	"""delete locations? from collection
+
+	NOT IMPLEMENTED 
+	"""
 	raise NotImplementedError('Deleting from collections has not been implemented')
 
 	if not isinstance(feature_ids, list):
@@ -136,6 +243,9 @@ def delete_from_collection(name, service, feature_ids):
 
 
 def _load_collections():
+	"""load list of collections
+
+	"""
 	path = os.path.join(util.get_dsl_dir(), COLLECTIONS_FILE)
 
 	if not os.path.exists(path):
@@ -146,11 +256,16 @@ def _load_collections():
 
 
 def _write_collection(collection):
+	"""write collection to json file
+
+	"""
 	with open(os.path.join(collection['path'], COLLECTION_METADATA_FILE), 'w') as f:
 		json.dump(collection, f)
 
 
 def _write_collections(collections):
+	"""write list of collections to json file 
+	"""
 	path = os.path.join(util.get_dsl_dir(), COLLECTIONS_FILE)
 	with open(path, 'w') as f:
 		json.dump(collections, f)
