e727719206815f0a6cabf06e0a2271bc0da12975
Kevin Winters <Kevin.D.Winters@erdc.dren.mil>
2015-05-14 14:45:24 -0500

    Fix missing comma

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         'numpy',
         'fiona>=1.5.1',
-        'rasterio>=0.23.0'
+        'rasterio>=0.23.0',
         'geojson',
         'jsonschema',
         'stevedore',
