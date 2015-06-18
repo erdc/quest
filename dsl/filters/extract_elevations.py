9a902e4183cfa7bf7c5f76dd371e860caed8c05c
Dharhas Pothina <dharhas@gmail.com>
2015-06-18 17:40:29 -0500

    generalize filter to use any simple vector format supported by fiona. Polygon, LineString, MultiPoint should work.

--- dsl/filters/extract_elevations.py
+++ dsl/filters/extract_elevations.py
@@ -11,6 +11,7 @@ import math
 import pandas as pd
 import os
 import subprocess
+import uuid
 
 
 class ExtractElevations(FilterBase):
@@ -37,89 +38,82 @@ class ExtractElevations(FilterBase):
 
     def apply_filter(self, collection_name, service=None, method='nearest', input_file=None, **kwargs):
         import rasterio
+        import fiona
 
         # convert service name to service code
         services = api.get_services(parameter='elevation', datatype='raster')
         service = [s['service_code'] for s in services if s['display_name']==service][0]
 
-        # calculate bounding box
-        print 'opening geojson file'
-        line = geojson.load(open(input_file))
-        if line['type'].lower()!='linestring':
-            print 'Please enter a GeoJSON formatted as a LineString'
-            return
-
-        coords = line['coordinates']
-        #polygon = Polygon([coords + list(reversed(coords))[1:]])
-        
-        coords = pd.DataFrame(coords, columns=['lon','lat'])
-
-        buf = 0.0001
-        bbox = [coords.lon.min()-buf, coords.lat.min()-buf, coords.lon.max()+buf, coords.lat.max()+buf]
-        x1, y1, x2, y2 = bbox
-        polygon = Polygon([_bbox2poly(x1, y1, x2, y2)])
-        print 'calculated bounding box: ', str(bbox)
-
-        # identify and download the required raster tiles
-        print 'downloading required tiles'
-        locs = api.get_locations(service, bounding_box=bbox)
-        locs = [loc['id'] for loc in locs['features']]
-        if len(locs)==0:
-            print 'no tiles found'
-            return
-
-        tiles = api.get_data(service, locs)
-        tiles = [v['elevation'] for v in tiles.values()]
-
-        if len(tiles)>1:
-            raster = os.path.splitext(tiles[0])[0] + '.vrt'
-            print subprocess.check_output(['gdalbuildvrt', '-overwrite', raster] + tiles)
-        else:
-            raster = tiles[0]
-
-        print 'extracting elevations along path'
-        with rasterio.drivers():
-            with rasterio.open(raster) as src:
-                if method=='nearest':
-                    elevations = list(src.sample(line['coordinates']))
-                    coords = [xy + z.tolist() for xy, z in zip(line['coordinates'], elevations)]
-                elif method=='bilinear':
-                    coords = []
-                    for x,y in line['coordinates']:
-                        a, b, c, d, e, f, _, _, _ = src.affine
-                        yf, r = math.modf((y-f)/e)
-                        xf, c = math.modf((x-c)/a)
-                        r, c = int(r), int(c)
-                        window = ((r, r+2), (c, c+2))
-                        data = src.read(None, window=window, masked=False, boundless=True)
-                        q00, q01, q10, q11 = data[0,0,0], data[0,1,0], data[0,0,1], data[0,1,1]
-                        z = (1-yf)*((1-xf)*q00 + xf*q10) + yf*((1-xf)*q01 + xf*q11)
-                        coords.append([x, y, z.tolist()])
-
-        line['coordinates'] = coords
-
-        #save GeoJSON to an output file and add to collection
+        #setup output file
         collection = api.get_collection(collection_name)
-
         plugin_dir = os.path.join(collection['path'], 'elevations_along_path')
         util.mkdir_if_doesnt_exist(plugin_dir)
         head, tail = os.path.split(input_file)
         fname, ext = os.path.splitext(tail)
         fname = fname + '_with_elevations_from_%s' % service + ext
         filename = os.path.join(plugin_dir, fname)
-        print 'saving output GeoJSON to %s' % filename
-        with open(filename, 'w') as f:
-            f.write(geojson.dumps(line))
+
+        # read vector features
+        with fiona.open(input_file, 'r') as vector:
+            # identify and download the required raster tiles
+            bbox = list(vector.bounds)
+            x1, y1, x2, y2 = bbox
+            polygon = Polygon([_bbox2poly(x1, y1, x2, y2)])
+
+            print 'downloading required tiles'
+            locs = api.get_locations(service, bounding_box=bbox)
+            locs = [loc['id'] for loc in locs['features']]
+            if len(locs)==0:
+                print 'no tiles found'
+                return
+
+            tiles = api.get_data(service, locs)
+            tiles = [v['elevation'] for v in tiles.values()]
+
+            if len(tiles)>1:
+                raster_file = os.path.splitext(tiles[0])[0] + '.vrt'
+                print subprocess.check_output(['gdalbuildvrt', '-overwrite', raster_file] + tiles)
+            else:
+                raster_file = tiles[0]
+
+            print 'extracting elevations along feature'
+            with rasterio.drivers():
+                with rasterio.open(raster_file, 'r') as raster:
+                    if os.path.isfile(filename):
+                        os.remove(filename) #fiona can't write to an existing file
+                    with fiona.open(filename,'w',driver=vector.driver, crs=vector.crs, schema=vector.schema) as output:
+                        for feature in vector:
+                            points = feature['geometry']['coordinates']
+                            if method=='nearest':
+                                elevations = list(raster.sample(points))
+                                coordinates = [xy + tuple(z.tolist()) for xy, z in zip(points, elevations)]
+                            elif method=='bilinear':
+                                coordinates = []
+                                for x,y in points:
+                                    a, b, c, d, e, f, _, _, _ = src.affine
+                                    yf, r = math.modf((y-f)/e)
+                                    xf, c = math.modf((x-c)/a)
+                                    r, c = int(r), int(c)
+                                    window = ((r, r+2), (c, c+2))
+                                    data = src.read(None, window=window, masked=False, boundless=True)
+                                    q00, q01, q10, q11 = data[0,0,0], data[0,1,0], data[0,0,1], data[0,1,1]
+                                    z = (1-yf)*((1-xf)*q00 + xf*q10) + yf*((1-xf)*q01 + xf*q11)
+                                    coordinates.append((x, y, z))
+
+                            feature['geometry']['coordinates'] = coordinates
+                            output.write(feature)
+
+            print 'saving output to %s' % filename
 
         properties = {
             'metadata': 'generated using get_elevations_along_path plugin', 
             'input_file': input_file,
             'elevation_service': service,
             'relative_path': filename, 
-            'datatype': 'linestring'
+            'datatype': 'vector',
         }
 
-        new_locs = FeatureCollection([Feature(geometry=polygon, properties=properties, id=str(hash(str(line))))])
+        new_locs = FeatureCollection([Feature(geometry=polygon, properties=properties, id=str(uuid.uuid4()))])
         collection = api.add_to_collection(collection_name, 'local', new_locs, parameters='elevation')
         return collection
 
