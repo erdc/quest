07632c7e97a59196f0973c5e5773196c7f98c107
Dharhas Pothina <dharhas@gmail.com>
2015-09-22 15:47:57 -0500

    clean up unused imports

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-from past.builtins import execfile
-import os
-import sys
-
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+import sys
 
 
 class PyTest(TestCommand):
