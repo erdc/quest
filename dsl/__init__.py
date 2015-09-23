564641a9c721d0ab120727575223fba97ee79dda
Dharhas Pothina <dharhas@gmail.com>
2015-09-22 23:05:33 -0500

    put py 2/3 compat imports in __init__.py

--- dsl/__init__.py
+++ dsl/__init__.py
@@ -4,7 +4,9 @@
 
     A library for environmental data services. Part of the Environmental Simulator project.
 """
-from __future__ import absolute_import
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
 from . import util, api
 
 # set version number
