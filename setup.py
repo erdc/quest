6287931d8a6e4dbaee3bb2ede0fd9286da2c4218
Dharhas Pothina <dharhas@gmail.com>
2015-09-23 14:01:43 -0500

    use pbr in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
 import sys
-
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -15,67 +14,7 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
-# this sets __version__
-# Parse the version from the fiona module.
-with open('dsl/__init__.py', 'r') as f:
-    for line in f:
-        if line.find("__version__") >= 0:
-            version = line.split("=")[1].strip()
-            version = version.strip("'")
-            continue
-
-
-with open('README.rst') as f:
-    # use README for long description but don't include the link to travis-ci;
-    # it seems a bit out of place on pypi since it applies to the development
-    # version
-    long_description = ''.join([
-        line for line in f.readlines()
-        if 'travis-ci' not in line])
-
-
 setup(
-    name='dsl',
-    version=version,
-    license='',
-    author='Dharhas Pothina',
-    author_email='dharhas.pothina@erdc.dren.mil',
-    description='Environmental Simulator, Data Services, Interpolation, Web Services',
-    long_description=long_description,
-    url='',
-    keywords='',
-    packages=find_packages(),
-    platforms='any',
-    install_requires=[
-        'numpy',
-        'fiona>=1.5.1',
-        'rasterio>=0.23.0',
-        'geojson',
-        'json-rpc',
-        'jsonrpc-requests',
-        'future',
-        'jsonschema',
-        'stevedore',
-        'ulmo>=0.8.1',
-        'pyyaml',
-        'pyoos',
-        'click',
-        'matplotlib>=1.4.0',
-        'werkzeug',
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: :: ',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-
-    include_package_data=True,
-    zip_safe=False,
-    tests_require=[
-        'pytest>=2.3.2',
-    ],
-    cmdclass={'test': PyTest},
+    setup_requires=['pbr'],
+    pbr=True,
 )
