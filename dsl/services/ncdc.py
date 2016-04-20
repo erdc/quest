112bc843bae33534c98c9a464ec5e5bd18e76152
Dharhas Pothina <dharhas@gmail.com>
2016-04-20 13:16:02 -0500

    implement downloads for ncdc closes #19

--- dsl/services/ncdc.py
+++ dsl/services/ncdc.py
@@ -1,9 +1,12 @@
 """DSL wrapper for NCDC GHCN and GSOD Services."""
 from .base import WebServiceBase
+from .. import util
 import pandas as pd
+import os
 from ulmo.ncdc import ghcn_daily, gsod
-from ulmo.ncdc.ghcn_daily.core import _get_inventory as _get_ghcn_inventory
+# from ulmo.ncdc.ghcn_daily.core import _get_inventory as _get_ghcn_inventory
 
+BASE_PATH = 'ncdc'
 
 class NcdcService(WebServiceBase):
     def _register(self):
@@ -51,6 +54,8 @@ class NcdcService(WebServiceBase):
     def _get_features(self, service):
         if service == 'ghcn-daily':
             features = ghcn_daily.get_stations(as_dataframe=True)
+            # these fields are causing problems when saving to database
+            features = features.drop(['USAF', 'WBAN'], axis=1)
 
         if service == 'gsod':
             features = gsod.get_stations()
@@ -95,7 +100,7 @@ class NcdcService(WebServiceBase):
 
         return parameters
 
-    def _parameter_map(self, service):
+    def _parameter_map(self, service, invert=False):
         if service=='ghcn-daily':
             pmap = {
                 'PRCP': 'rainfall:daily:total',
@@ -115,10 +120,103 @@ class NcdcService(WebServiceBase):
                 'max_temp': 'air_temperature:daily:min',
             }
 
+        if invert:
+            pmap = {v: k for k, v in pmap.items()}
+
         return pmap
 
-    def _download(self, path, service, feature, **kwargs):
-        pass
+    def _download(self, service, feature, save_path, dataset,
+                  parameter, start=None, end=None):
+
+        if end is None:
+            end = pd.datetime.now().strftime('%Y-%m-%d')
+
+        if start is None:
+            start = pd.to_datetime(end) - pd.datetools.timedelta(days=30)
+            start = start.strftime('%Y-%m-%d')
+
+        pmap = self._parameter_map(service, invert=True)
+        parameter_code = pmap[parameter]
+
+        if service == 'ghcn-daily':
+            data = ghcn_daily.get_data(feature,
+                                       elements=parameter_code,
+                                       as_dataframe=True)[parameter_code]
+            if data.empty:
+                raise ValueError('No Data Available')
+
+            data = data[start:end]
+            data.rename(columns={'value': parameter}, inplace=True)
+
+        if service == 'gsod':
+            data = gsod.get_data(feature, start=start, end=end,
+                                 parameters=parameter_code)[feature]
+            data = pd.DataFrame(data)
+            if data.empty:
+                raise ValueError('No Data Available')
+
+            data = data.set_index('date')
+            data.index = pd.PeriodIndex(data.index, freq='D')
+            data.rename(columns={parameter_code: parameter}, inplace=True)
+
+        save_path = os.path.join(save_path, BASE_PATH, service)
+        save_path = os.path.join(save_path, dataset)
+        metadata = {
+            'save_path': save_path,
+            'file_format': 'hdf5',
+            'datatype': 'timeseries',
+            'parameter': parameter,
+            'units': self._unit_map(service)[parameter],
+            'feature': 'svc://ncdc:{}/{}'.format(service, feature)
+        }
+
+        # save data to disk
+        io = util.load_drivers('io', 'timeseries-hdf5')
+        io = io['timeseries-hdf5'].driver
+        io.write(save_path, data, metadata)
+
+        return metadata
 
     def _download_options(self, service):
-        pass
+        schema = {
+            "title": "NCDC Download Options",
+            "type": "object",
+            "properties": {
+                "parameter": {
+                    "type": "string",
+                    "enum": self._parameter_map(service).values(),
+                    "description": "start date",
+                },
+                "start": {
+                    "type": "string",
+                    "description": "start date",
+                },
+                "end": {
+                    "type": "string",
+                    "description": "end date",
+                },
+            },
+        }
+        return schema
+
+    def _unit_map(self, service):
+        if service == 'ghcn-daily':
+            umap = {
+                'rainfall:daily:total': '0.1*mm',
+                'snowfall:daily:total': 'mm',
+                'snow_depth:daily:total': 'mm',
+                'air_temperature:daily:total': '0.1*degC',
+                'air_temperature:daily:minimum': '0.1*degC',
+                'air_temperature:daily:mean': '0.1*degC',
+            }
+
+        if service == 'gsod':
+            umap = {
+                'rainfall:daily:total': '0.01*inches',
+                'snow_depth:daily:total': '0.01*inches',
+                'air_temperature:daily:max': '0.1*degF',
+                'air_temperature:daily:min': '0.1*degF',
+                'air_temperature:daily:min': '0.1*degF',
+            }
+
+        return umap
