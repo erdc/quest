686c95f2f5a40bacabde8322d2f6283e87d48733
Dharhas Pothina <dharhas@gmail.com>
2016-06-08 13:24:43 -0500

    fix packaging and finding of non .py data files

--- dsl/__init__.py
+++ dsl/__init__.py
@@ -9,6 +9,16 @@ from __future__ import (absolute_import, division,
 from builtins import *
 from . import util, api
 import pbr.version
+import os
 
 # set version number
 __version__ = pbr.version.VersionInfo('dsl').version_string_with_vcs()
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_pkg_data_path(filename=None):
+    """Return path to dsl package data directory."""
+    if filename is None:
+        filename = ''
+
+    return os.path.join(_ROOT, 'data', filename)
