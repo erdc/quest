2481b3661fd347e3b0bb6c8caccaba60d180e159
Dharhas Pothina <dharhas@gmail.com>
2016-06-14 15:22:57 -0500

    WIP: base datalibrary filter

--- dsl/filters/datalibrary/datalibrary.py
+++ dsl/filters/datalibrary/datalibrary.py
@@ -1,9 +1,13 @@
 """Functions required to assemble and run plugins from the C++ DataLibrary."""
 from ..base import FilterBase
 from dsl import get_pkg_data_path, util
-from dsl.api import get_metadata, new_dataset, update_metadata
+from dsl.api import get_metadata, new_dataset, update_metadata, new_feature
+from dsl.api.projects import active_db
 
+from geojson import Polygon
 from jinja2 import Environment, FileSystemLoader
+import json
+import numpy as np
 import os
 from subprocess import check_output
 import tempfile
@@ -20,12 +24,12 @@ class DatalibraryBase(FilterBase):
         self.metadata = {
             'group': 'vitd',
             'operates_on': {
-                'datatype': ['vitd-*'],
+                'datatype': ['vitd'],
                 'geotype': None,
                 'parameters': None,
             },
             'produces': {
-                'datatype': ['nrmm'],
+                'datatype': None,
                 'geotype': None,
                 'parameters': None,
             },
@@ -35,68 +39,67 @@ class DatalibraryBase(FilterBase):
     def apply_filter(self, datasets, features=None, options=None,
                      display_name=None, description=None, metadata=None):
 
-        if len(datasets) > 1:
-            raise NotImplementedError('This filter can only be applied to a single dataset')
+        datasets = util.listify(datasets)
+
+        # get metadata, path etc from first dataset, i.e. assume all datasets
+        # are in same folder. This will break if you try and combine datasets
+        # from different services
+        dataset = datasets[0]
+        orig_metadata = get_metadata(dataset)[dataset]
 
         bbox = _get_bbox(datasets)
-        src = _get_src(datasets)
-
-        io = util.load_drivers('io', 'timeseries-hdf5')
-        io = io['timeseries-hdf5'].driver
+        src = _get_src(dataset)
         orig_metadata = get_metadata(dataset)[dataset]
-        if orig_metadata['_save_path'] is None:
-            raise IOError('No data file available for this dataset')
+        display_name = 'Created by filter {}'.format(self.name)
+        geom_coords = [util.bbox2poly(*bbox)]
+        cname = orig_metadata['_collection']
+        feature = new_feature(cname,
+                              display_name=feature_name, geom_type='Polygon',
+                              geom_coords=geom_coords)
 
-        df = io.read(orig_metadata['_save_path'])
-
-        #apply transformation
-        if options is None:
-            options = {}
-
-        # run filter
-        new_df = self._apply(df, options)
-
-        # setup new dataset
-        new_metadata = {
-            'parameter': new_df.metadata.get('parameter'),
-            'datatype': orig_metadata['_datatype'],
-            'filter_applied': self.name,
-            'filter_options': options,
-            'parent_datasets': dataset,
-            'file_format': orig_metadata['_file_format'],
-            'timezone': new_df.metadata.get('timezone'),
-            'units': new_df.metadata.get('units'),
-        }
-
-        if description is None:
-            description = 'TS Filter Applied'
-
-        new_dset = new_dataset(orig_metadata['_feature'],
+        new_dset = new_dataset(feature,
                                dataset_type='derived',
                                display_name=display_name,
                                description=description)
 
-        # save dataframe
-        save_path = os.path.split(orig_metadata['_save_path'])[0]
-        save_path = os.path.join(save_path, new_dset)
-        io.write(save_path, new_df, new_metadata)
+        # define dst folder as collection/dataset_name
+        prj = os.path.dirname(active_db())
+        dst = os.path.join(prj, cname, new_dset)
+        util.mkdir_if_doesnt_exist(dst)
 
-        new_metadata.update({'save_path': save_path})
+        if options is None:
+            options = {}
+
+        options.update({'src': src, 'dst': dst})
+        _run_filter(self.name, **options)
+
+        new_metadata = self._new_dataset_metadata()
+
+        if description is None:
+            description = 'VITD Filter Applied'
+
+        # update metadata
+        new_metadata.update({
+            'filter_applied': self.name,
+            'filter_options': options,
+            'parent_datasets': ','.join(datasets),
+            # 'save_path': dst,
+        })
+
         metadata = update_metadata(new_dset,
                                    dsl_metadata=new_metadata,
                                    metadata=metadata)
 
         return metadata
 
-    def apply_filter_options(self, **kwargs):
-        schema = {}
-
-        return schema
-
-    def _apply(df, metadata, options):
+    def apply_filter_options(self):
         raise NotImplementedError
 
-def render_template(name, **kwargs):
+    def _new_dataset_metadata():
+        raise NotImplementedError
+
+
+def _render_template(name, **kwargs):
     if 'bbox' in kwargs.keys():
         xmin, ymin, xmax, ymax = util.listify(kwargs['bbox'])
         kwargs['bbox'] = '{} {} {} {}'.format(ymax, xmin, ymin, xmax)
@@ -105,9 +108,9 @@ def render_template(name, **kwargs):
     return template.render(**kwargs)
 
 
-def run_filter(name, **kwargs):
+def _run_filter(name, **kwargs):
     with tempfile.NamedTemporaryFile(delete=False) as t:
-        t.write(render_template(name, **kwargs))
+        t.write(_render_template(name, **kwargs))
         fname = t.name
 
     runplugin = os.environ.get('DSL_RUNPLUGIN')
@@ -116,3 +119,23 @@ def run_filter(name, **kwargs):
     output = check_output([runplugin, fname])
     print(output)
     return output
+
+
+def _get_bbox(datasets):
+    f = get_metadata(datasets, as_dataframe=True)['_feature'].tolist()
+    geom_coords = get_metadata(f, as_dataframe=True)['_geom_coords'].tolist()
+    coords = []
+    for gc in geom_coords:
+        coords.append(np.array(json.loads(gc)))
+
+    coords = np.hstack(coords).squeeze()
+    xmax, ymax = coords.max(axis=0)
+    xmin, ymin = coords.min(axis=0)
+    return [xmin, ymin, xmax, ymax]
+
+
+def _get_src(dataset):
+    save_path = get_metadata(dataset)[dataset].get('_save_path')
+    save_path = util.listify(save_path)[0]
+    # the root vitd directory is three levels above file name
+    return os.path.split(os.path.split(os.path.split(save_path)[0])[0])[0]
