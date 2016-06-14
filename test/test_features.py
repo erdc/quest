6ccdd33c2cf7e7796827d59e0cca8cd61298a5b7
LaKenya K Walker <LaKenya.K.Walker@erdc.dren.mil>
2016-06-14 09:12:30 -0500

    Updated example base_dir

--- test/test_features.py
+++ test/test_features.py
@@ -1,12 +1,16 @@
 import dsl
-
+import os
 
 def _setup():
-    dsl.api.update_settings({'BASE_DIR': 'files/example_base_dir'})
+    path = os.path.dirname(os.path.realpath(__file__)) + '/files/example_base_dir'
+    dsl.api.update_settings({'BASE_DIR': path})
+    dsl.api.set_active_project('project1')
 
-def _teardown(uri):
-    dsl.api.delete(uri)
-    dsl.api.new_collection(uri)
+def _teardown():
+    dsl.api.delete(dsl.api.get_collections())
+    dsl.api.new_collection('col1')
+    dsl.api.new_collection('col2')
+    dsl.api.new_collection('col3')
 
 def test_add_features():
     _setup()
@@ -14,28 +18,29 @@ def test_add_features():
     c = dsl.api.get_features(collections='col1')
     assert len(list(c)) == 1
     assert b == c
-    _teardown('col1')
+    _teardown()
 
 def test_get_features():
     _setup()
     dsl.api.add_features(collection='col2', features='svc://usgs-nwis:iv/01529500, svc://usgs-nwis:iv/01529950')
     c=dsl.api.get_features(collections='col2')
     assert len(list(c)) == 2
-    _teardown('col2')
+    _teardown()
 
 def test_new_feature():
     _setup()
     c = dsl.api.new_feature(collection='col3', display_name='NewFeat', geom_type='LineString')
     assert dsl.api.get_metadata(c)[c]['_display_name'] == 'NewFeat'
     assert c in dsl.api.get_features(collections='col3')
-    _teardown('col3')
+    _teardown()
 
 def test_update_feature():
-     _setup()
-     assert dsl.api.get_metadata('col2')['col2']['_description'] == None
-     dsl.api.update_metadata('col2', description='test collection2')
-     assert dsl.api.get_metadata('col2')['col2']['_description'] == "['test collection2']"
-     _teardown('col2')
+    _setup()
+    dsl.api.update_metadata('col2', description='test collection2')
+    assert dsl.api.get_metadata('col2')['col2']['_description'] == "['test collection2']"
+    dsl.api.update_metadata('col2', description='test_collection2_sample')
+    assert dsl.api.get_metadata('col2')['col2']['_description'] == "['test_collection2_sample']"
+    _teardown()
 
 
 def test_delete_features():
@@ -45,4 +50,4 @@ def test_delete_features():
     dsl.api.delete(c)
     assert len(dsl.api.get_features(collections='col1')) == 1
     assert d in dsl.api.get_features(collections='col1')
-    _teardown('col1')
+    _teardown()
--- test/test_projects.py
+++ test/test_projects.py
@@ -6,63 +6,57 @@ import tempfile
 import os
 
 
+def _setup():
+    path = os.path.dirname(os.path.realpath(__file__)) + '/files/example_base_dir'
+    dsl.api.update_settings({'BASE_DIR': path})
+    dsl.api.set_active_project('project1')
 
-
+def _teardown(uri):
+    dsl.api.delete_project(uri,True)
 
 def test_get_projects():
-    dsl.api.update_settings({'BASE_DIR': 'files/example_base_dir'})
+    _setup()
     c = dsl.api.get_projects()
-    assert len(list(c)) == 2
-
-
+    assert len(c) == 3
+#
 def test_default_project():
     _setup()
     c = dsl.api.get_projects()
-    assert len(list(c)) == 1
     assert 'default' in c
-    #assert c['default']['display_name'] == 'Default Project'
 
 
 def test_new_project():
     _setup()
     dsl.api.new_project('test')
     c = dsl.api.get_projects()
-    assert len(list(c)) == 2
-
-
+    assert len(c) == 4
+    _teardown('test')
+#
+#
 def test_add_project():
     _setup()
-    dsl.api.new_project('test1')
-    dsl.api.new_project('test2')
-    dsl.api.new_project('test3')
-    dsl.api.delete_project('test2')
-    dsl.api.delete_project('default')
     path=os.getcwd()
-    dsl.api.add_project('project1', path+'/files/example_base_dir/projects/project1')
+    dsl.api.add_project('added_test', path+'/test/files/sample_project_dir/projects/addedTest')
     c = dsl.api.get_projects()
-    assert len(list(c)) == 3
-
+    assert len(list(c)) == 4
+    _teardown('added_test')
 
 def test_delete_project():
     _setup()
-    dsl.api.new_project('test1')
-    dsl.api.new_project('test2')
-    dsl.api.new_project('test3')
-    dsl.api.delete_project('test2')
-    dsl.api.delete_project('default')
+    dsl.api.new_project('test_project')
     c = dsl.api.get_projects()
-    assert len(list(c)) == 2
+    assert len(c) == 4
+    dsl.api.delete_project('test_project')
+    c=dsl.api.get_projects()
+    assert len(c) == 3
+    _teardown('test_project')
 
 
-def set_active_project():
+def test_set_active_project():
     _setup()
-    assert dsl.api.get_active_project() == 'default'
+    assert dsl.api.get_active_project() == 'project1'
     dsl.api.new_project('test1')
     dsl.api.set_active_project('test1')
     assert dsl.api.get_active_project() == 'test1'
+    _teardown('test1')
 
-
-def _setup():
-    tmpdir = tempfile.mkdtemp()
-    dsl.api.update_settings({'BASE_DIR': tmpdir})
-    return tmpdir
