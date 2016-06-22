422b75cc5a76949877bc102ed010801aa4b95c7d
Scott D. Christensen <sdc50@byu.net>
2016-06-22 09:36:57 -0500

    Updated tests to work with fixtures. Two tests are failing because of changes that need to be made to the code: test_delete_project[default], test_get_cache_data_dir.

--- test/test_collections.py
+++ test/test_collections.py
@@ -1,25 +1,18 @@
 import dsl
-import os
-import tempfile
-import shutil
+import pytest
 
-def _setup():
-    path = os.path.dirname(os.path.realpath(__file__)) + '/files/example_base_dir'
-    dsl.api.update_settings({'BASE_DIR': path})
-    dsl.api.set_active_project('project2')
+ACTIVE_PROJECT = 'project2'
 
+pytestmark = pytest.mark.usefixtures('reset_projects_dir', 'set_active_project')
 
-def _teardown():
-    dsl.api.delete(dsl.api.get_collections())
 
 def test_get_collections():
-    _setup()
     dsl.api.set_active_project('project1')
     c = dsl.api.get_collections()
     assert len(list(c)) == 3
 
+
 def test_new_collection():
-    _setup()
     c = dsl.api.new_collection('test1')
     assert len(list(dsl.api.get_collections())) == 1
     metadata = {
@@ -31,10 +24,9 @@ def test_new_collection():
     c = dsl.api.get_collections()
     assert 'test1' in c
     assert 'test2' in c
-    _teardown()
+
 
 def test_delete():
-    _setup()
     dsl.api.new_collection('test1')
     dsl.api.new_collection('test2')
     dsl.api.new_collection('test3')
@@ -44,14 +36,13 @@ def test_delete():
 
     dsl.api.delete('test3')
     assert len(list(dsl.api.get_collections())) == 1
-    _teardown()
+
 
 def test_update_collection():
-    _setup()
     dsl.api.new_collection('test1')
     metadata = {'display_name': 'New Name', 'new_field': 'test'}
 
     c = dsl.api.update_metadata('test1', metadata=metadata)
     assert c['test1']['display_name'] == 'New Name'
     assert c['test1']['new_field'] == 'test'
-    _teardown()
+
--- test/test_projects.py
+++ test/test_projects.py
@@ -4,66 +4,72 @@
 import dsl
 import tempfile
 import os
+import pytest
+import shutil
+
+ACTIVE_PROJECT = 'project1'
+
+pytestmark = pytest.mark.usefixtures('reset_projects_dir')
 
 
-def _setup():
-    path = os.path.dirname(os.path.realpath(__file__)) + '/files/example_base_dir'
-    dsl.api.update_settings({'BASE_DIR': path})
-    dsl.api.set_active_project('project1')
+@pytest.fixture()
+def init_project_to_add(request):
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    project_template_dir = os.path.join(base_path, 'files', 'project_to_add_template')
+    project_dir = os.path.join(base_path, 'files', 'project_to_add')
 
-def _teardown(proj):
-    dsl.api.delete_project(proj,True)
+    def handle_error(function, path, excinfo):
+        print("Ignoring error", function, path, excinfo)
 
-def test_get_projects():
-    _setup()
+    def cleanup():
+        shutil.rmtree(project_dir, ignore_errors=False, onerror=handle_error)
+    cleanup()
+
+    shutil.copytree(project_template_dir, project_dir)
+
+    request.addfinalizer(cleanup)
+
+    return project_dir
+
+
+@pytest.fixture(params=['project1', 'default'])
+def test_project(request):
+    return request.param
+
+
+def test_get_projects(reset_projects_dir):
     c = dsl.api.get_projects()
-    assert len(c) == 3
-#
+    assert len(c) == reset_projects_dir['NUMBER_OF_PROJECTS']
+
+
 def test_default_project():
-    _setup()
     c = dsl.api.get_projects()
     assert 'default' in c
 
 
-def test_new_project():
-    _setup()
-    dsl.api.delete_project('test')
+def test_new_project(reset_projects_dir):
     dsl.api.new_project('test')
     c = dsl.api.get_projects()
-    assert len(c) == 4
-    _teardown('test')
-#
-
-def test_add_project():
-    path = os.path.dirname(os.path.realpath(__file__))+ '/files/sample_project_dir'
-    if not path:
-        print "Path doesnt exist"
-    dsl.api.update_settings({'BASE_DIR': path})
-    dsl.api.new_project('newtest')
-    _setup()
-    dsl.api.add_project('added_test', path + '/projects/newtest')
-    c=dsl.api.get_projects()
-    assert len(list(c)) == 4
-    _teardown('added_test')
-    dsl.api.update_settings({'BASE_DIR': path})
-    _teardown('newtest')
+    assert len(c) == reset_projects_dir['NUMBER_OF_PROJECTS'] + 1
+    assert 'test' in c
 
 
-def test_delete_project():
-    _setup()
-    dsl.api.new_project('test_project')
+def test_add_project(reset_projects_dir, init_project_to_add):
+    added_project_name = 'added_test_project'
+    dsl.api.add_project(added_project_name, init_project_to_add)
     c = dsl.api.get_projects()
-    assert len(c) == 4
-    dsl.api.delete_project('test_project',True)
-    c=dsl.api.get_projects()
-    assert len(c) == 3
+    assert len(list(c)) == reset_projects_dir['NUMBER_OF_PROJECTS'] + 1
+    assert added_project_name in c
 
 
-def test_set_active_project():
-    _setup()
+def test_delete_project(reset_projects_dir, test_project):
+    dsl.api.delete_project(test_project, True)
+    c = dsl.api.get_projects()
+    assert len(c) == reset_projects_dir['NUMBER_OF_PROJECTS'] - 1
+    assert test_project not in c
+
+
+def test_set_active_project(set_active_project):
     assert dsl.api.get_active_project() == 'project1'
-    dsl.api.new_project('test1')
-    dsl.api.set_active_project('test1')
-    assert dsl.api.get_active_project() == 'test1'
-    _teardown('test1')
-
+    dsl.api.set_active_project('project2')
+    assert dsl.api.get_active_project() == 'project2'
