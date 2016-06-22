5aacc72cfd2f87c9654d05662b377ed9873442a8
Scott D. Christensen <sdc50@byu.net>
2016-06-22 09:36:57 -0500

    Updated tests to work with fixtures. Two tests are failing because of changes that need to be made to the code: test_delete_project[default], test_get_cache_data_dir.

---
+++ test/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+import os
+import shutil
+
+import dsl
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.join(base_path, 'files/example_base_dir')
+
+
+@pytest.fixture(scope='session')
+def set_base_dir():
+    dsl.api.update_settings({'BASE_DIR': BASE_DIR})
+    return BASE_DIR
+
+
+
+@pytest.fixture()
+def reset_projects_dir(set_base_dir, request):
+    projects_dir = os.path.join(BASE_DIR, 'projects')
+
+    def handle_error(function, path, excinfo):
+        print("Ignoring error", function, path, excinfo)
+
+    def cleanup():
+        shutil.rmtree(projects_dir, ignore_errors=False, onerror=handle_error)
+
+    projects_template_dir = os.path.join(BASE_DIR, 'projects_template')
+    shutil.copytree(projects_template_dir, projects_dir)
+
+    request.addfinalizer(cleanup)
+
+    metadata = {'NUMBER_OF_PROJECTS': 4}
+    return metadata
+
+
+@pytest.fixture()
+def set_active_project(set_base_dir, request):
+    previous_active_project = dsl.api.get_active_project()
+    tests_active_project = getattr(request.module, "ACTIVE_PROJECT", "default")
+    dsl.api.set_active_project(tests_active_project)
+
+    def teardown():
+        dsl.api.set_active_project(previous_active_project)
+
+    request.addfinalizer(teardown)
+
+
