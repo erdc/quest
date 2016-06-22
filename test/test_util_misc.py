89bc1f6c44dbbee3f72e4e6437a8bb7240d2cd38
Scott D. Christensen <sdc50@byu.net>
2016-06-22 15:52:17 -0500

    Bugfixes to test fixtures

--- test/test_util_misc.py
+++ test/test_util_misc.py
@@ -2,18 +2,18 @@ import dsl
 import os
 import tempfile
 
-def test_get_dsl_dir():
-    dsl.api.update_settings(config={'BASE_DIR': 'dsl'})
-    assert dsl.util.get_dsl_dir() == 'dsl'
+from conftest import BASE_DIR
 
 
-def test_get_cache_data_dir():
-    dsl.api.update_settings(config={'BASE_DIR': 'dsl'})
+def test_get_dsl_dir(reset_projects_dir):
+    assert dsl.util.get_dsl_dir() == BASE_DIR
 
-    assert dsl.util.get_cache_dir() == os.path.join('dsl', 'cache')
-    assert dsl.util.get_projects_dir() == os.path.join('dsl', 'projects')
+
+def test_get_cache_data_dir(reset_projects_dir):
+    assert dsl.util.get_cache_dir() == os.path.join(BASE_DIR, 'cache')
+    assert dsl.util.get_projects_dir() == os.path.join(BASE_DIR, 'projects')
 
     folder = tempfile.gettempdir()
-    dsl.api.update_settings(config={'BASE_DIR': 'dsl', 'CACHE_DIR': folder, 'PROJECTS_DIR': folder})
+    dsl.api.update_settings(config={'CACHE_DIR': folder, 'PROJECTS_DIR': folder})
     assert dsl.util.get_cache_dir() == folder
     assert dsl.util.get_projects_dir() == folder
