cf4964c3f6e6ad9d21946ff7b70d6449ca19b804
Dharhas Pothina <dharhas@gmail.com>
2016-12-30 13:47:47 -0600

    finish implementing tasks api

--- dsl/api/tasks.py
+++ dsl/api/tasks.py
@@ -1,87 +1,120 @@
-from functools import wraps, partial
+from functools import wraps
 from distributed import Client, LocalCluster
 import psutil
-import uuid
+from jsonrpc import dispatcher
+import pandas as pd
+from ..util import listify
 
 
 _ncores = psutil.cpu_count()
-
-cluster = LocalCluster(nanny=False, n_workers=_ncores)
-client = Client(cluster)
-
+_client = None
 tasks = {}
+
+
+def _get_client():
+    global _client
+    if _client is None:
+        cluster = LocalCluster(nanny=False, n_workers=_ncores)
+        _client = Client(cluster)
+    return _client
+
 
 def add_async(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         async = kwargs.pop('async', None)
         if async:
-            task_id = uuid.uuid4().hex
+            client = _get_client()
             future = client.submit(f, *args, **kwargs)
             task = {
-                'task': f.__name__,
+                'fn': f.__name__,
                 'args': args,
                 'kwargs': kwargs,
                 'future': future,
-                'status': 'active',
+                'status': future.status,
                 'result': None,
             }
-            tasks[task_id] = task
-            future.add_done_callback(partial(_completed, task_id=task_id))
-            return task_id
+            tasks[future.key] = task
+            future.add_done_callback(_completed)
+            return future.key
         else:
             return f(*args, **kwargs)
     return wrapper
 
 
-def get_task(task_id):
-    task = tasks.get(task_id)
-    if task:
-        task['task_id'] = task_id
-        return task
-    print('task {} not found'.format(task_id))
-    return
+@dispatcher.add_method
+def get_task(task_id, with_future=None):
+    task = get_tasks(expand=True, with_future=with_future).get(task_id)
+    if task is None:
+        print('task {} not found'.format(task_id))
+    return task
 
 
-def get_task_result(task_id):
-    task = get_task(task_id)
-    return task.get('result')
-
-
-def get_task_status(task_id):
-    task = get_task(task_id)
-    return task.get('status')
-
-
-def cancel_task(task_id):
-    task = get_task(task_id)
-
-
-def list_tasks(task=None, status=None, expand=None):
+@dispatcher.add_method
+def get_tasks(filters=None, expand=None, as_dataframe=None, with_future=None):
     """
-        task = function name
-        status = active, completed
+        filters: any of the dict keys i.e. status, fn
+        status = (pending, cancelled, finished, lost, error)
     """
+    # update status
+    _update_status()
+
     task_list = tasks
-    if status:
-        task_list = {k: v for k, v in task_list.items() if v['status'] == status}
+    if filters:
+        for fk, fv in filters.items():
+            task_list = {k: v for k, v in task_list.items() if v[fk] == fv}
 
-    if task:
-        task_list = {k: v for k, v in task_list.items() if v['task'] == task}
+    task_list = pd.DataFrame.from_dict(task_list, orient='index')
+    if not with_future:
+        task_list.drop(['future'], inplace=True, axis=1, errors='ignore')
 
-    if not expand:
-        task_list = task_list.keys()
+    if not expand and not as_dataframe:
+        task_list = task_list.index.tolist()
+
+    if expand:
+        task_list = task_list.to_dict()
 
     return task_list
 
 
-def _completed(future, task_id):
-    tasks[task_id]['status'] = 'completed'
-    tasks[task_id]['result'] = future.result()
+@dispatcher.add_method
+def cancel_tasks(task_ids):
+    task_ids = listify(task_ids)
+    df = get_tasks(with_future=True, as_dataframe=True)
+    futures = df['future'][task_ids].tolist()
+    c = _get_client()
+    c.cancel(futures)
     return
 
 
-def _pretty_print(f, args, kwargs):
-    args_str = ", ".join('"{}"'.format(x) if isinstance(x, str) else str(x) for x in args)
-    kwargs_str = ", ".join(['{}={}'.format(k, v) for k, v in kwargs.items()])
-    return '{}({}{})'.format(f.__name__, args_str, kwargs_str)
+@dispatcher.add_method
+def remove_tasks(status=None):
+    """
+        status can be a list of statuses, default removes
+        tasks with status = ['cancelled', 'finished', 'lost', 'error']
+        from task list
+    """
+    global tasks
+    _update_status()
+    if status:
+        status = listify(status)
+    else:
+        status = ['cancelled', 'finished', 'lost', 'error']
+
+    if 'pending' in status:
+        print('cannot remove pending tasks, please cancel them first')
+        status.remove('pending')
+
+    tasks = {k: v for k, v in tasks.items() if v['status'] in status}
+    return
+
+
+def _completed(future):
+    tasks[future.key]['status'] = future.status
+    tasks[future.key]['result'] = future.result()
+    return
+
+
+def _update_status():
+    for k in tasks.keys():
+        tasks[k]['status'] = tasks[k]['future'].status
