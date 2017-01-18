a31bce00e80c67d0b6bcaaf0d9d3a4593099cb5e
LaKenya K Walker <LaKenya.K.Walker@erdc.dren.mil>
2017-01-18 11:33:10 -0600

    Updated tests to work with dbRefactor

--- dsl/api/metadata.py
+++ dsl/api/metadata.py
@@ -140,8 +140,8 @@ def update_metadata(uris, display_name=None, description=None,
         elif not isinstance(description, list):
             raise ValueError('description must be a list if more that one uri is passed in')
 
-        # if not isinstance(metadata, list):
-        #     metadata = [metadata] * n
+        if not isinstance(metadata, list):
+            metadata = [metadata] * n
 
         if not isinstance(dsl_metadata, list):
                     dsl_metadata = [dsl_metadata] * n
@@ -166,7 +166,7 @@ def update_metadata(uris, display_name=None, description=None,
 
         db = get_db()
         with db_session:
-            if resource == 'collection':
+            if resource == 'collections':
                 entity = db.Collection[uri]
             elif resource == 'features':
                 entity = db.Feature[uri]
