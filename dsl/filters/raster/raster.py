8cdb4f725eda121b929cd8ef0b7dd51171036ba3
LaKenya K Walker <LaKenya.K.Walker@erdc.dren.mil>
2017-01-18 11:33:10 -0600

    Updated tests to work with dbRefactor

--- dsl/filters/raster/raster.py
+++ dsl/filters/raster/raster.py
@@ -50,40 +50,40 @@ class RstUnitConversion(RstBase):
     def apply_filter_options(self, fmt, **kwargs):
        raise NotImplementedError
 
-class RstMerge(RstBase):
-        def register(self, name='raster-merge-datasets'):
-            RstBase.register(self, name=name)
-
-        def _apply(self, df, options):
-            df = df.read()
-            unitsMap = {
-                "ft3/s": "cu_ft/s",
-
-            }
-            metadata = options.get('orig_meta')
-            if 'save_path' in metadata:
-                del metadata['save_path']
-
-            reg = UnitRegistry()
-            from_units = metadata['_units']
-            if from_units is None:
-                raise NotImplementedError('This dataset does not contain units')
-            if from_units not in dir(reg):
-                from_units = unitsMap[from_units]
-            if '/' in from_units and '/' not in options.get('to_units'):
-                beg = from_units.find('/')
-                end = len(from_units)
-                default_time = from_units[beg:end]
-                to_units = options.get('to_units') + default_time
-            conversion = reg.convert(1, src=from_units, dst=options.get('to_units'))
-            df[df.columns[0]] = df[df.columns[0]] * conversion
-            metadata.update({'units': options.get('to_units')})
-            df.metadata = metadata
-
-            return from_units
-
-        def apply_filter_options(self, fmt, **kwargs):
-            raise NotImplementedError
+# class RstMerge(RstBase):
+        # def register(self, name='raster-merge-datasets'):
+        #     RstBase.register(self, name=name)
+        #
+        # def _apply(self, df, options):
+        #     df = df.read()
+        #     unitsMap = {
+        #         "ft3/s": "cu_ft/s",
+        #
+        #     }
+        #     metadata = options.get('orig_meta')
+        #     if 'save_path' in metadata:
+        #         del metadata['save_path']
+        #
+        #     reg = UnitRegistry()
+        #     from_units = metadata['_units']
+        #     if from_units is None:
+        #         raise NotImplementedError('This dataset does not contain units')
+        #     if from_units not in dir(reg):
+        #         from_units = unitsMap[from_units]
+        #     if '/' in from_units and '/' not in options.get('to_units'):
+        #         beg = from_units.find('/')
+        #         end = len(from_units)
+        #         default_time = from_units[beg:end]
+        #         to_units = options.get('to_units') + default_time
+        #     conversion = reg.convert(1, src=from_units, dst=options.get('to_units'))
+        #     df[df.columns[0]] = df[df.columns[0]] * conversion
+        #     metadata.update({'units': options.get('to_units')})
+        #     df.metadata = metadata
+        #
+        #     return from_units
+        #
+        # def apply_filter_options(self, fmt, **kwargs):
+        #     raise NotImplementedError
 
 class RstClipPolygon(RstBase):
     def register(self, name='raster-clip-polygon'):
