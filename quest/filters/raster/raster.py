a2302169a074ecdaaf3b2ba9af9827be013bc36c
LaKenya K Walker <LaKenya.K.Walker@erdc.dren.mil>
2017-02-27 15:59:59 -0600

    watershed init commit

--- quest/filters/raster/raster.py
+++ quest/filters/raster/raster.py
@@ -59,11 +59,16 @@ class RstUnitConversion(RstBase):
 
 
 
-class RstD8(RstBase):
-    def register(self, name='rst-d8'):
+class RstWaterShed(RstBase):
+    def register(self, name='rst-watershed-delineation'):
                 RstBase.register(self, name=name)
 
     def _apply(self, df, options):
+
+                dem = df.read().squeeze()
+                filterResult = terrapin.d8(dem)
+                filterResult.shape = (1,) + filterResult.shape
+                df = filterResult
 
                 return df
 
