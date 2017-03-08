72e6a652f9952cdf095dd43223fe39959ec96e72
LaKenya K Walker <LaKenya.K.Walker@erdc.dren.mil>
2017-03-08 16:55:22 -0600

    initial commit for watershed and flow accumulation filter

--- quest/filters/raster/rst_base.py
+++ quest/filters/raster/rst_base.py
@@ -82,6 +82,13 @@ class RstBase(FilterBase):
             'parameter': orig_metadata['parameter'],
             'datatype': orig_metadata['datatype'],
             'file_format': orig_metadata['file_format'],
+        }
+
+        if description is None:
+            description = 'Raster Filter Applied'
+
+        # update metadata
+        new_metadata.update({
             'options': self.options,
             'file_path': self.file_path,
             'status': datasets.DatasetStatus.FILTERED,
