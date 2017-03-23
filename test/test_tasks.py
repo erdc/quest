a8ef924a1ea3fef5a8ee0350549f9766bf619210
Scott D. Christensen <sdc50@byu.net>
2017-03-23 17:09:18 -0500

    task tests with rpc (broken)

--- test/test_tasks.py
+++ test/test_tasks.py
@@ -25,6 +25,8 @@ def long_process_with_exception(delay, msg):
     1/0
     return {'delay': delay, 'msg': msg}
 
+setattr(quest.api, 'long_process', long_process)
+setattr(quest.api, 'long_process_with_exception', long_process_with_exception)
 
 setattr(quest.api, 'long_process', long_process)
 setattr(quest.api, 'long_process_with_exception', long_process_with_exception)
